3670c6f3bf8ed67d5c75b624b9fbc495f423343d
mellow0614 <mellow0614@naver.com>
2021-07-19 17:49:47 +0900

    애완동물 dto생성 - [조동찬 / 2021.07.19] - PetDto, NoteDto 생성

---
+++ src/main/java/com/pet/care/dto/PetDto.java
@@ -0,0 +1,147 @@
+package com.pet.care.dto;
+
+import java.util.Date;
+
+public class PetDto {
+
+	private String id;
+	private String user_email;
+	private String name;
+	private Date birth;
+	private int weight;
+	private String gender;
+	private String type;
+	private String typeinfo;
+	private String aniregistnum;
+	private String notification;
+	private String profile;
+	private String delflag;
+	
+	public PetDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PetDto(String id, String user_email, String name, Date birth, int weight, String gender, String type,
+			String typeinfo, String aniregistnum, String notification, String profile, String delflag) {
+		super();
+		this.id = id;
+		this.user_email = user_email;
+		this.name = name;
+		this.birth = birth;
+		this.weight = weight;
+		this.gender = gender;
+		this.type = type;
+		this.typeinfo = typeinfo;
+		this.aniregistnum = aniregistnum;
+		this.notification = notification;
+		this.profile = profile;
+		this.delflag = delflag;
+	}
+
+	@Override
+	public String toString() {
+		return "PetDto [id=" + id + ", user_email=" + user_email + ", name=" + name + ", birth=" + birth + ", weight="
+				+ weight + ", gender=" + gender + ", type=" + type + ", typeinfo=" + typeinfo + ", aniregistnum="
+				+ aniregistnum + ", notification=" + notification + ", profile=" + profile + ", delflag=" + delflag
+				+ "]";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTypeinfo() {
+		return typeinfo;
+	}
+
+	public void setTypeinfo(String typeinfo) {
+		this.typeinfo = typeinfo;
+	}
+
+	public String getAniregistnum() {
+		return aniregistnum;
+	}
+
+	public void setAniregistnum(String aniregistnum) {
+		this.aniregistnum = aniregistnum;
+	}
+
+	public String getNotification() {
+		return notification;
+	}
+
+	public void setNotification(String notification) {
+		this.notification = notification;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public String getDelflag() {
+		return delflag;
+	}
+
+	public void setDelflag(String delflag) {
+		this.delflag = delflag;
+	}
+	
+	
+}
