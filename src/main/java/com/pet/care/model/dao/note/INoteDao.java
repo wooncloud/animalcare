d062a1de0096abb80ac3be2a14ee3792dee4357a
mellow0614 <mellow0614@naver.com>
2021-07-23 16:59:45 +0900

    건강수첩 개발 - [조동찬 / 2021.07.23] - 건강수첩 리스트 jstree - pet 기능 일별 리스트 조회 추가

--- src/main/java/com/pet/care/model/dao/note/INoteDao.java
+++ src/main/java/com/pet/care/model/dao/note/INoteDao.java
@@ -1,5 +1,18 @@
 package com.pet.care.model.dao.note;
 
+import java.util.List;
+import java.util.Map;
+
+import com.pet.care.dto.NoteDto;
+
 public interface INoteDao {
 
+	//건강수첩 리스트 조회 
+	public List<NoteDto> noteList(Map<String, Object> map);
+	
+	//건강수첩 일별 리스트 조회
+	public List<NoteDto> selNoteList(Map<String, Object> map);
+	
+	//건강수첩 상세조회
+	public NoteDto noteDetail(int seq);
 }
--- src/main/java/com/pet/care/model/service/note/NoteServiceImpl.java
+++ src/main/java/com/pet/care/model/service/note/NoteServiceImpl.java
@@ -1,8 +1,36 @@
 package com.pet.care.model.service.note;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.pet.care.dto.NoteDto;
+import com.pet.care.model.dao.note.INoteDao;
 
 @Service
 public class NoteServiceImpl implements INoteService {
 
+	@Autowired
+	private INoteDao iNoteDao;
+	
+	@Override
+	public List<NoteDto> noteList(Map<String, Object> map) {
+		return iNoteDao.noteList(map);
+	}
+
+	@Override
+	public List<NoteDto> selNoteList(Map<String, Object> map) {
+		return iNoteDao.selNoteList(map);
+	}
+
+	@Override
+	public NoteDto noteDetail(int seq) {
+		// TODO Auto-generated method stub
+		return iNoteDao.noteDetail(seq);
+	}
+
+
+	
 }
